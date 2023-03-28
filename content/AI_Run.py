c0dcc802f1a47814270aa96d1dc668af5079c02b
Hưng Lê <112607056+thoriumrabbit@users.noreply.github.com>
2023-03-28 15:14:43 +0700

    Threading push

--- content/AI_Run.py
+++ content/AI_Run.py
@@ -3,20 +3,44 @@ from PIL import Image, ImageOps
 import numpy as np
 import cv2
 import time
-
+import multiprocessing
+import threading
+import signal
 
 
 cam = cv2.VideoCapture(0)
 current_directory_path = os.path.abspath(os.path.join(__file__, "..\..\content"))
-def image_capture():
-    ret,frame = cam.read()
-    cv2.imwrite("{0}\input.png".format(current_directory_path),frame)
 
+def ai_capture():
+    os.system(cmd)
 
 print('__file__:    ', current_directory_path)
-cmd = "python {0}\yolov5/detect.py --source {0}\input.png --weights {0}\yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --nosave".format(current_directory_path)   
+cmd = "python {0}\yolov5/detect_custom.py --source 0 --weights {0}\yolov5/runs/train/exp/weights/best.pt --img 640 --conf 0.25 --nosave --hide-conf".format(current_directory_path)   
 
-for num in range(1, 11):
-    image_capture()
-    os.system(cmd)
+
+
+signal = 0
+t1 = threading.Timer(0, ai_capture)
+
+
+
+def handle_signal(sig):
+    global signal, t1
+    if sig == 1 and not t1.is_alive():
+        t1.start()
+    elif sig == 0:
+        signal = 0
+        t1.finished.set()
+
+            
+
+while True:
+    # replace this with your logic to receive the signal from the server
+    signal = signal^1
+    print(signal)
+    handle_signal(signal)
+    time.sleep(10)
+    t1.finished.set()
+
+
     
